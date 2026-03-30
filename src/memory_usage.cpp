b997f380d7c64a5f5ec9ad6f4a13b5db6de0c20b
Yuhan Cao <916042768@qq.com>
2026-03-30 09:47:07 +0800

    添加Linux适配

--- src/memory_usage.cpp
+++ src/memory_usage.cpp
@@ -1,17 +1,22 @@
 #include "pfind_external_sorting/monitor/memory_usage.h"
 
-#include <windows.h>
-#include <psapi.h>  //paspi.h需要用到windows.h中的内容，需要在windows.h之后引入
-
 #include <cstdint>
 #include <iostream>
 
 #include "pfind_external_sorting/model.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#include <psapi.h>  //paspi.h需要用到windows.h中的内容，需要在windows.h之后引入
+#else
+#include <sys/resource.h>
+#endif
+
 namespace external_sort {
 namespace monitor {
 // 获取内存使用情况
 void PrintMemoryUsage() {
+#ifdef _WIN32
   model::MemoryUsage memory{};
 
   PROCESS_MEMORY_COUNTERS_EX pmc;
@@ -28,7 +33,12 @@ void PrintMemoryUsage() {
   // peakRss=" << memory.peak_rss / 1024.0 / 1024.0 << "MB" << '\n';
   std::cout << "峰值内存使用量 " << memory.peak_rss / 1024.0 / 1024.0 << "MB"
             << '\n';
-
+#else
+  struct rusage usage {};
+  if (getrusage(RUSAGE_SELF, &usage) == 0) {
+    std::cout << "峰值内存使用量 " << usage.ru_maxrss / 1024.0 << "MB" << '\n';
+  }
+#endif
   return;
 }
 
